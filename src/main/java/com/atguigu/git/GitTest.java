520531a4c779ad3201599f17bbdbab22edd450d6
0x_Jss <0x_Jss@qq.com>
2021-12-31 16:50:38 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author 0x_Jss
+ * @date 2021年12月31日 16:43
+ * @description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git!");
+    }
+}
